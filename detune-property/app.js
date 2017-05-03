f9d2595ed50a92e36a25be5ca3da250634dd70a8
Geralex <geralexndr@gmail.com>
2017-05-03 11:05:32 +0300

    The following code plays a note at a frequency of 130.81 hertz and is the frequency of a C3 note. The oscillator stops, and a half-second later a second note plays with the same frequency.value and a detune.value of 100 cents, making the note value C#3.

---
+++ detune-property/app.js
@@ -0,0 +1,16 @@
+var audioContext = new AudioContext();
+var oscillator = audioContext.createOscillator();
+oscillator.connect(audioContext.destination);
+oscillator.frequency.value = 130.81; //________C3
+oscillator.start(audioContext.currentTime);
+oscillator.stop(audioContext.currentTime + 2);
+oscillator.onended = function() {
+    oscillator = audioContext.createOscillator();
+    oscillator.frequency.value = 130.81; // C3 note
+    oscillator.detune.value = 100;
+    /*sets the note to one half step
+     higher to C#3*/
+    oscillator.connect(audioContext.destination);
+    oscillator.start(audioContext.currentTime + 0.5);
+    oscillator.stop(audioContext.currentTime + 2.5);
+};
