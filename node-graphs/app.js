123c38f5f8a3341ac6a2cee528ae1c568b893b49
Geralex <geralexndr@gmail.com>
2017-05-03 10:09:03 +0300

    The following code is an example of an oscillator node connected to a filter node.

---
+++ node-graphs/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var audioContext = new AudioContext();
+
+//_____________BEGIN create oscillator and filter
+var oscillator = audioContext.createOscillator();
+var filter = audioContext.createBiquadFilter();
+//_____________END create oscillator and filter
+
+//______________BEGIN connect oscillator to filter
+oscillator.connect(filter);
+//_____________END connect oscillator to filter
+
+//_____________BEGIN connect filter to computer speakers
+filter.connect(audioContext.destination);
+//_____________END connect filter to computer speakers
+
+//_____________BEGIN start oscillator playing
+oscillator.start(audioContext.currentTime);
+//_____________END start oscillator playing
