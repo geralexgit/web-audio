d708670326b8384f3402babe23dd42fdae0bf802
Geralex <geralexndr@gmail.com>
2017-05-03 09:26:57 +0300

    The following code uses a method named getNameAndArtist of an object named song and applies it to an object named Album.

---
+++ bind/app.js
@@ -0,0 +1,19 @@
+var album = {
+    name: "Funky Shuffle",
+    artist: "James Jackson",
+    format: "wave",
+    sampleRate: 44100
+};
+
+var song = {
+    name: "Analogue Heaven",
+    artist: "The Keep It Reels",
+    getNameAndArtist: function () {
+        return "Name: " + this.name + " | Artist: " + this.artist;
+    }
+};
+
+var getNameOfAlbum = song.getNameAndArtist.bind(album);
+console.log(getNameOfAlbum());
+/*Name: Funky Shuffle | Artist:
+ James Jackson*/
